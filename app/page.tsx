8437b1945512a124adde34c3415f599093130ac2
Thomas Potherat <t.potherat@laposte.net>
2026-09-21 17:41:18 +0200

    added button to scroll on top quickly

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Footer from "@/components/Footer";
 import { NavbarDemo } from "@/components/Header";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
+import ScrollToTop from "@/components/ScrollToTop";
 import ExpertiseSection from "@/components/Services";
 import Stack from "@/components/Stack";
 
@@ -18,6 +19,7 @@ export default function Home() {
         <Cta />
       </main>
       <Footer />
+      <ScrollToTop />
     </div>
   );
 }
